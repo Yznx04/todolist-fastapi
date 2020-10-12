b599f7ab6ca0c12e370d1514d68288c689701e64
Yznx04 <2687927133@qq.com>
2020-10-12 21:39:57 +0800

    新增模块路由以及创建表

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ from fastapi import FastAPI
 
 from user.views import router
 
-api = FastAPI()
+api = FastAPI(tags=["home"])
 api.include_router(router=router, prefix="/user", tags=["User"])
+
+
+@api.get("/")
+def home():
+    return {"yznx": "Keep coding every days!"}
